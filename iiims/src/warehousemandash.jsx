f9ff35410de5135bf3406ae76ff891560f39a924
sudhar277 <cb.en.u4cse21661@cb.students.amrita.edu>
2024-04-04 14:37:08 +0530

    table styling and user story 4 completed

--- iiims/src/warehousemandash.jsx
+++ iiims/src/warehousemandash.jsx
@@ -1,21 +1,19 @@
-import  { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import profilepic from "./default-profile-picture.jpg"
 import { getRecentProducts } from './Components/apiservice';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const DashboardPage = () => {
-  const [inventoryData, setInventoryData] = useState([
-    { inventory_id: '', product_id: '', import_: '', export: '', location: '' }
-  ]);
+  const [inventoryData, setInventoryData] = useState([{ inventory_id: '', product_id: '', import_: '', export: '', location: '' }]);
   const [showUpdatePage, setShowUpdatePage] = useState(true);
   const [showHistoryPage, setShowHistoryPage] = useState(false);
   const [showInventoryPage, setShowInventoryPage] = useState(false); // New state for showing inventory
   const [errors, setErrors] = useState({});
   const [inventoryList, setInventoryList] = useState([]); // State to hold fetched inventory data
   const [recentProducts, setRecentProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-  //for view inventory
+  // Fetching recent products
   useEffect(() => {
     const fetchRecentProducts = async () => {
       try {
@@ -29,9 +27,8 @@ const DashboardPage = () => {
     fetchRecentProducts();
   }, [showInventoryPage]);
 
-  const [products, setProducts] = useState([]);
-
-  const getproducts = async () => {
+  // Fetching products
+  const getProducts = async () => {
     const response = await fetch('https://inventory-db-m6j0.onrender.com/products', {
       method: 'GET',
       headers: {
@@ -44,28 +41,24 @@ const DashboardPage = () => {
     }
 
     const data = await response.json();
-    console.log('Products:', data); // Log the fetched data
     setProducts(data);
-    console.log('Products:', products); // Log the fetched data
   };
 
   useEffect(() => {
-    getproducts();
+    getProducts();
   }, []);
 
-
-
-  // Dummy product names
-  // const dummyProducts = ["Dummy Product 1", "Dummy Product 2", "Dummy Product 3"];
-
+  // Handle adding a new row
   const handleAddRow = () => {
     setInventoryData(prevData => [...prevData, { inventory_id: '', product_id: '', import_: '', export: '', location: '' }]);
   };
 
+  // Handle removing a row
   const handleRemoveRow = (indexToRemove) => {
     setInventoryData(prevData => prevData.filter((_, index) => index !== indexToRemove));
   };
 
+  // Handle input change in the form
   const handleInputChange = (index, event) => {
     const { name, value } = event.target;
     const newData = [...inventoryData];
@@ -89,11 +82,9 @@ const DashboardPage = () => {
         break;
     }
     setInventoryData(newData);
-    //console.log(inventoryData);
   };
 
-
-
+  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
     let errors = {};
@@ -104,12 +95,12 @@ const DashboardPage = () => {
         isValid = false;
       }
     });
-  
+
     if (!isValid) {
       setErrors(errors);
       return;
     }
-  
+
     // Send data to the API
     try {
       const response = await fetch('https://inventory-db-m6j0.onrender.com/inventory_product', {
@@ -119,56 +110,55 @@ const DashboardPage = () => {
         },
         body: JSON.stringify(inventoryData)
       });
-  
+
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      alert(JSON.stringify(data)); 
-  
+      alert(JSON.stringify(data));
+
       // Clear errors if the form is successfully submitted
       setErrors({});
-  
+
       // Reset form fields
-      setInventoryData([
-        { inventory_id: '', product_id: '', import_: '', export: '', location: '' }
-      ]);
+      setInventoryData([{ inventory_id: '', product_id: '', import_: '', export: '', location: '' }]);
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
+  // Show update page
   const handleShowUpdatePage = () => {
     setShowUpdatePage(true);
     setShowHistoryPage(false);
-    setShowInventoryPage(false); // Hide inventory page
+    setShowInventoryPage(false);
   };
 
+  // Show history page
   const handleShowHistoryPage = () => {
     setShowUpdatePage(false);
     setShowHistoryPage(true);
-    setShowInventoryPage(false); // Hide inventory page
+    setShowInventoryPage(false);
   };
 
+  // Show inventory page
   const handleShowInventoryPage = () => {
     setShowInventoryPage(true);
     setShowUpdatePage(false);
     setShowHistoryPage(false);
-    // Here you can make an API call to fetch inventory data from the backend and update `inventoryList` state
-    // For now, let's assume `inventoryList` is empty
     setInventoryList([]);
   };
 
+  // Logout function
   const handleLogout = () => {
-    // Implement your logout logic here
-    // For now, let's simply reload the page
     window.location.reload();
   };
 
-  const [producthistory, setProductHistory] = useState([]);
+  // Fetch product history
+  const [productHistory, setProductHistory] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState('');
 
-  const getproducthistory = async (product_id) => {
+  const getProductHistory = async (product_id) => {
     const response = await fetch('https://inventory-db-m6j0.onrender.com/view_history', {
       method: 'POST',
       headers: {
@@ -176,25 +166,21 @@ const DashboardPage = () => {
       },
       body: JSON.stringify({ product_id: Number(product_id) })
     });
-  
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
-      return 1;
     }
-  
+
     const data = await response.json();
     setProductHistory(data);
-    console.log(producthistory);
     setSelectedProduct(product_id);
-    return 1;
-    
-  }
+  };
+
   useEffect(() => {
     if (showHistoryPage) {
-      getproducthistory("0");
+      getProductHistory("0");
     }
   }, [showHistoryPage]);
-
 
   return (
     <div style={{ backgroundColor: '#f5f5f5', minHeight: '100vh', fontFamily: 'Arial, sans-serif', display: 'flex', flexDirection: 'column' }}>
@@ -220,45 +206,45 @@ const DashboardPage = () => {
         </div>
       </nav>
       <section style={{ paddingTop: '50px', paddingRight: '200px', paddingLeft: '200px', flexGrow: 1 }}>
-      {showHistoryPage &&  (
+        {showHistoryPage && (
           <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', boxShadow: '0 2px 4px rgba(0,0,0,0.1)' }}>
             <h2 style={{ color: '#4285F4', marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>View History</h2>
-            <select 
-                name="productname" 
-                onChange={(e) => getproducthistory(e.target.value || "0")} 
-                style={{ marginRight: '10px', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}
-              >
-                <option value="">Select Product</option>
-                {Object.entries(products).map(([id, name], i) => (
-                  <option key={i} value={id}>{name}</option>
+            <select
+              name="productname"
+              onChange={(e) => getProductHistory(e.target.value || "0")}
+              style={{ marginRight: '10px', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}
+            >
+              <option value="">Select Product</option>
+              {Object.entries(products).map(([id, name], i) => (
+                <option key={i} value={id}>{name}</option>
+              ))}
+            </select>
+            <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid #ccc' }}>
+              <thead>
+                <tr>
+                  <th style={{ border: '1px solid #ccc' }}>Product ID</th>
+                  <th style={{ border: '1px solid #ccc' }}>Date of transaction </th>
+                  <th style={{ border: '1px solid #ccc' }}>Import</th>
+                  <th style={{ border: '1px solid #ccc' }}>Export</th>
+                  <th style={{ border: '1px solid #ccc' }}>Total Quantity</th>
+                  <th style={{ border: '1px solid #ccc' }}>Inventory ID</th>
+                  <th style={{ border: '1px solid #ccc' }}>Location</th>
+                </tr>
+              </thead>
+              <tbody>
+                {productHistory.map((product, index) => (
+                  <tr key={index}>
+                    <td style={{ border: '1px solid #ccc' }}>{product.product_id}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.datetime}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.import}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.export}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.total_quantity}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.inventory_id}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.location}</td>
+                  </tr>
                 ))}
-              </select>
-              <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-      <thead>
-        <tr>
-          <th>Product ID</th>
-          <th>Date of transaction </th>
-          <th>Import</th>
-          <th>Export</th>
-          <th>Total_Quantity</th>
-          <th>Inventory ID</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {producthistory.map((product, index) => (
-          <tr key={index}>
-            <td>{product.product_id}</td>
-            <td>{product.datetime}</td>
-            <td>{product.import}</td>
-            <td>{product.export}</td>
-            <td>{product.total_quantity}</td>
-            <td>{product.inventory_id}</td>
-            <td>{product.location}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              </tbody>
+            </table>
           </div>
         )}
         {showUpdatePage && (
@@ -268,7 +254,6 @@ const DashboardPage = () => {
               {inventoryData.map((data, index) => (
                 <div key={index} style={{ marginBottom: '15px', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>
                   <input type="number" name="inventoryid" placeholder="Inventory ID" min="0" value={data.inventory_id} onChange={(e) => handleInputChange(index, e)} style={{ marginRight: '10px', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }} />
-                  {/* Dropdown for product name */}
                   <select name="productname" value={data.product_id} onChange={(e) => handleInputChange(index, e)} style={{ marginRight: '10px', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}>
                     <option value="">Select Product</option>
                     {Object.entries(products).map(([id, name], i) => (
@@ -288,32 +273,32 @@ const DashboardPage = () => {
           </div>
         )}
         {showInventoryPage && (
-  <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', boxShadow: '0 2px 4px rgba(0,0,0,0.1)' }}>
-    <h2 style={{ color: '#4285F4', marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>View Inventory</h2>
-    <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-      <thead>
-        <tr>
-          <th>Product ID</th>
-          <th>Product Name</th>
-          <th>Quantity</th>
-          <th>Inventory ID</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {recentProducts.map((product, index) => (
-          <tr key={index}>
-            <td>{product.product_id}</td>
-            <td>{product.product_name}</td>
-            <td>{product.quantity}</td>
-            <td>{product.inventory_id}</td>
-            <td>{product.location}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-)}
+          <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', boxShadow: '0 2px 4px rgba(0,0,0,0.1)' }}>
+            <h2 style={{ color: '#4285F4', marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>View Inventory</h2>
+            <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid #ccc' }}>
+              <thead>
+                <tr>
+                  <th style={{ border: '1px solid #ccc' }}>Product ID</th>
+                  <th style={{ border: '1px solid #ccc' }}>Product Name</th>
+                  <th style={{ border: '1px solid #ccc' }}>Quantity</th>
+                  <th style={{ border: '1px solid #ccc' }}>Inventory ID</th>
+                  <th style={{ border: '1px solid #ccc' }}>Location</th>
+                </tr>
+              </thead>
+              <tbody>
+                {recentProducts.map((product, index) => (
+                  <tr key={index}>
+                    <td style={{ border: '1px solid #ccc' }}>{product.product_id}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.product_name}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.quantity}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.inventory_id}</td>
+                    <td style={{ border: '1px solid #ccc' }}>{product.location}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </section>
     </div>
   );
